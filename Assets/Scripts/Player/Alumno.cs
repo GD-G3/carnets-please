ac96c4de9a8801bcebf7ae7c591e18ffa6223d86
Cinver Espinoza <149912156+AlemEsv@users.noreply.github.com>
2026-06-11 21:55:24 -0500

    feat Agregar Clase alumno

---
+++ Assets/Scripts/Player/Alumno.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Alumno
+{
+    public string nombre;
+    public string codigo;
+    public string fecha_vencimiento;
+    public Sprite foto;
+    public string facultad;
+    public string carrera;
+}
